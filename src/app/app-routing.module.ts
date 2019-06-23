f6c3fc87bec363feb06bae5d64813eebe96ef78f
Jeffrey Hamlin <sourcesage@gmail.com>
2019-06-23 15:09:20 -0400

    Auto logout, navigates to Login screen, navigation updates.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { LoginComponent } from './authentication/login.component';
 import { SignupComponent } from './authentication/signup.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { HomeComponent } from './home/home.component';
+import { LogoutComponent } from './logout/logout.component';
 
 const routes: Routes = [
   {path: '', component: WelcomeComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'logout', component: LogoutComponent},
+  { path: '**', redirectTo: '/login' }
 ];
 
 @NgModule({
